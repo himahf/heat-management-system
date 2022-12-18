e984a4d84a5bf6f82472b610fcd5f691a99b5995
Ishara_Sandun <isharasandun31@gmail.com>
2022-12-18 20:45:36 +0100

    Initial Commit - Add Connection

--- app/src/main/java/com/example/heat_manager/MainActivity.java
+++ app/src/main/java/com/example/heat_manager/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.heat_manager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -20,10 +18,20 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,11 +55,57 @@ public class MainActivity extends AppCompatActivity {
     Switch editTemp;
 
     int commonTempVal = 10;
+    String output;
+
+    public void createConnection(String command) {
+        String hostname = "130.237.177.211";
+        String username = "pi";
+        String password = "IoT@2021";
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try
+        {
+
+            Connection conn = new Connection(hostname); //init connection
+
+            conn.connect(); //start connection to the hostname
+            boolean isAuthenticated = conn.authenticateWithPassword(username,
+                    password);
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+            Session sess = conn.openSession();
+            sess.execCommand(command);
+            InputStream stdout = new StreamGobbler(sess.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+//reads text
+            while (true){
+                String line = br.readLine(); // read line
+                if (line == null)
+                    break;
+                System.out.println(line);
+                output = line;
+            }
+            /* Show exit status, if available (otherwise "null") */
+            System.out.println("ExitCode: " + sess.getExitStatus());
+            sess.close(); // Close this session
+            conn.close();
+        }
+        catch (IOException e)
+        {
+            System.out.printf(e.toString());
+            e.printStackTrace(System.err);
+            System.exit(2); }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createConnection("python IOT_Project/TempSen17.py");
 //        sp=findViewById(R.id.SpCountry);
         UserName=(TextView) findViewById(R.id.userName);
         UserPassword= (TextView) findViewById(R.id.userPassword);
